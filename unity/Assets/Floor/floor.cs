9b968ff8bf48fbff05e79a4e223975bbc533b1d8
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 02:20:33 +0900

    Floor class & exit portal class added

---
+++ unity/Assets/Floor/floor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+class Floor : MonoBehaviour {
+    public int StageLevel;
+    public int FloorItemCount;
+
+    private void Start()
+    {
+        // TODO : opening event scene
+
+    }
+
+    private bool CheckCondition(Player player)
+    {
+        if (player.Inventory.Count == FloorItemCount) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void ProceedToNextFloor(Player player)
+    {
+        if (CheckCondition(player) == true) {
+            // TODO : show error message
+        }
+        else {
+            SceneManager.LoadScene("Stage" + (StageLevel + 1));
+        }
+    }
+}
