948f666a7dfd63eac5ec2188179560314b9a7152
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 03:04:56 +0900

    Script file rename

---
+++ unity/Assets/Script/Floor/Floor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+class Floor : MonoBehaviour {
+    public int StageLevel;
+    public int FloorItemCount;
+
+    private void Start()
+    {
+        // TODO : opening event scene
+
+    }
+
+    private bool CheckCondition(Player player)
+    {
+        return player.Inventory.Count == FloorItemCount;
+    }
+
+    public void ProceedToNextFloor(Player player)
+    {
+        if (CheckCondition(player) == false) {
+            // TODO : show error message
+            Debug.Log("Item count condition not met : Current item count = " + player.Inventory.Count + ", required item count = " + FloorItemCount);
+        }
+        else {
+            SceneManager.LoadScene("Stage" + (StageLevel + 1));
+        }
+    }
+}
