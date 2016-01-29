41ff620dbc7b889ecebf9ca7c233eaf64bf2df28
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 02:20:59 +0900

    Player class added

---
+++ unity/Assets/Player/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+class Player : MonoBehaviour {
+    private Inventory inventory;
+
+    public Inventory Inventory { get { return inventory; } }
+
+    private void Start()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+
+    public void AcquireItem(Item item)
+    {
+        inventory.AddItem(item);
+    }
+}
