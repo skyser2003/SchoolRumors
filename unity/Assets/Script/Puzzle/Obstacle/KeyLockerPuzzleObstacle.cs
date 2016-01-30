dc06fd4eb9b1068749a46e23b85fd68662fa6b81
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 22:17:29 +0900

    KeyLockerPuzzleObstacle

---
+++ unity/Assets/Script/Puzzle/Obstacle/KeyLockerPuzzleObstacle.cs
@@ -0,0 +1,17 @@
+﻿class KeyLockerPuzzleObstacle : PuzzleObstacle {
+    public KeyHandheldItem item;
+
+    public override void Action(Player player)
+    {
+        if (item == null) {
+            return;
+        }
+
+        if (player.HandheldItem == null || player.HandheldItem.GetType() != typeof(WrenchHandheldItem)) {
+            return;
+        }
+
+        item.GetPickedUp(player);
+        item = null;
+    }
+}
