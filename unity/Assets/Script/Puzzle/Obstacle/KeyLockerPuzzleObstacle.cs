dbd67cd12cd8080c7b21f07e728a6e117834459f
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 22:32:16 +0900

    Lockers have error text now

--- unity/Assets/Script/Puzzle/Obstacle/KeyLockerPuzzleObstacle.cs
+++ unity/Assets/Script/Puzzle/Obstacle/KeyLockerPuzzleObstacle.cs
@@ -1,5 +1,15 @@
-﻿class KeyLockerPuzzleObstacle : PuzzleObstacle {
+﻿using UnityEngine;
+
+class KeyLockerPuzzleObstacle : PuzzleObstacle {
+    private PlayerUI playerUI;
+
     public KeyHandheldItem item;
+    public string ErrorText;
+
+    protected override void Start()
+    {
+        playerUI = GameObject.FindWithTag("UI").GetComponent<PlayerUI>();
+    }
 
     public override void Action(Player player)
     {
@@ -8,6 +18,7 @@
         }
 
         if (player.HandheldItem == null || player.HandheldItem.GetType() != typeof(WrenchHandheldItem)) {
+            playerUI.SetErrorMessage(ErrorText, 2);
             return;
         }
 
--- unity/Assets/Script/Puzzle/Obstacle/LockerPuzzleObstacle.cs
+++ unity/Assets/Script/Puzzle/Obstacle/LockerPuzzleObstacle.cs
@@ -1,5 +1,15 @@
-﻿class LockerPuzzleObstacle : PuzzleObstacle {
+﻿using UnityEngine;
+
+class LockerPuzzleObstacle : PuzzleObstacle {
+    private PlayerUI playerUI;
+
     public RitualItem item;
+    public string ErrorText;
+
+    protected override void Start()
+    {
+        playerUI = GameObject.FindWithTag("UI").GetComponent<PlayerUI>();
+    }
 
     public override void Action(Player player)
     {
@@ -8,6 +18,7 @@
         }
 
         if (player.HandheldItem == null || player.HandheldItem.GetType() != typeof(WrenchHandheldItem)) {
+            playerUI.SetErrorMessage(ErrorText, 2);
             return;
         }
 
