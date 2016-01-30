dae582b147056258782730efc37f669b7e3fbb81
Samuli Jaaskelainen <jaaskelainen.samuli@gmail.com>
2016-01-31 01:57:59 +0900

    Title screen.

---
+++ unity/Assets/Script/UI/Title.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Title : MonoBehaviour
+{
+    public Sprite title1;
+    public Sprite title2;
+    Image image;
+
+    float timer;
+    float rate = 0.6f;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update ()
+    {
+	    if(Time.time > timer)
+        {
+            timer = Time.time + rate;
+
+            if(image.sprite == title1)
+            {
+                image.sprite = title2;
+            }
+            else
+            {
+                image.sprite = title1;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Stage");
+        }
+	}
+}
---
+++ unity/Assets/Script/UI/ToggleElement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ToggleElement : MonoBehaviour
+{
+    private bool visible = false; //show or hide at start
+    public GameObject go;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => {
+            OnClick();
+        });
+        OnClick(); //set initial state
+    }
+
+    void OnClick()
+    {
+        visible = !visible;
+        go.SetActive(!visible);
+    }
+}
