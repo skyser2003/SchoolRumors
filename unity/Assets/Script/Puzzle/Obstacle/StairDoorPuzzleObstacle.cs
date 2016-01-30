f4745bee9b4942fade59901a2a7d299838afdfb8
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 22:26:06 +0900

    Ritual item counting changed Added a door puzzle that requires all rituals on the floor

---
+++ unity/Assets/Script/Puzzle/Obstacle/StairDoorPuzzleObstacle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+class StairDoorPuzzleObstacle : PuzzleObstacle {
+    private PlayerUI playerUI;
+    private bool isOpen;
+
+    public string ConditionTextShow = "You need all ritual items to go to the next floor";
+
+    protected override void Start()
+    {
+        base.Start();
+        PuzzleObstacleManager.Instance.Add(this);
+        playerUI = GameObject.FindWithTag("UI").GetComponent<PlayerUI>();
+        isOpen = false;
+    }
+
+    public override void Action(Player player)
+    {
+        if (player.Inventory.Count != RitualItem.allItems.Count) {
+            playerUI.SetErrorMessage(ConditionTextShow, 2);
+            return;
+        }
+        else if (isOpen == false) {
+            isOpen = true;
+            transform.Rotate(0, 90, 0);
+        }
+    }
+}
