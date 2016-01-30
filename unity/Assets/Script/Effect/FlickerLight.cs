195895433947ba91c4344e5cb5542aba163664f0
Samuli Jaaskelainen <jaaskelainen.samuli@gmail.com>
2016-01-30 22:23:40 +0900

    Flicker light effect.

---
+++ unity/Assets/Script/Effect/FlickerLight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerLight : MonoBehaviour
+{
+    public float intensityVariation = 0.15f;
+    float minIntensity;
+    float maxIntensity;
+    public float speed = 1.5f;
+    Light fLight;
+
+    float random;
+
+    void Start()
+    {
+        fLight = GetComponent<Light>();
+        minIntensity = fLight.intensity - intensityVariation;
+        maxIntensity = fLight.intensity + intensityVariation;
+        random = Random.Range(0.0f, 65535.0f);
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time * speed);
+        fLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+    }
+}
