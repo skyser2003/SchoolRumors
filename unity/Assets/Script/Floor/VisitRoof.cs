ed0f2c1a73814c7de90df0a2c4afe591cc4eb3a0
Samuli Jaaskelainen <jaaskelainen.samuli@gmail.com>
2016-01-30 15:15:26 +0900

    Made enemies taller & added tooltips to ritual items.

---
+++ unity/Assets/Script/Floor/VisitRoof.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VisitRoof : MonoBehaviour
+{
+    public Text tooltip1;
+    public Text tooltip2;
+    public Text tooltip3;
+
+    void OnTriggerEnter (Collider col)
+    {
+	    if(col.transform.tag == "Player")
+        {
+            tooltip1.text = "Changed text 1";
+            tooltip2.text = "Changed text 2";
+            tooltip3.text = "Changed text 3";
+        }
+	}
+	
+}
