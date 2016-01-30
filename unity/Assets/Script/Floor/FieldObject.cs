2aa188e0f9ca0bf4cba97fbca6994e2ed993f9dc
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 14:22:36 +0900

    FieldObject added When action key(space) is pressed near FIeldObject, if it contains item then give it to the player

---
+++ unity/Assets/Script/Floor/FieldObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+class FieldObject : MonoBehaviour {
+    public Item item;
+
+    private void Start()
+    {
+        FieldObjectManager.Instance.Add(this);
+    }
+
+    public void GiveItem(Player player)
+    {
+        if (item == null) {
+            return;
+        }
+
+        player.AcquireItem(item);
+        item = null;
+    }
+}
---
+++ unity/Assets/Script/Player/PlayerAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+class PlayerAction : MonoBehaviour {
+    private Player player;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            var obj = FieldObjectManager.Instance.FindClosest(transform.position, 1);
+            if (obj != null) {
+                obj.GiveItem(player);
+            }
+        }
+    }
+}
