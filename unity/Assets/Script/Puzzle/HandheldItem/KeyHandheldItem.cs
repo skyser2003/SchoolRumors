35b0043ff38ca9c659c28cf83f9b84c0ea541340
Seung Ho Jang <shjang1992@gmail.com>
2016-01-30 18:21:05 +0900

    Unused code removed

--- unity/Assets/Script/Puzzle/HandheldItem/KeyHandheldItem.cs
+++ unity/Assets/Script/Puzzle/HandheldItem/KeyHandheldItem.cs
@@ -1,14 +1,2 @@
-﻿using UnityEngine;
-
-class KeyHandheldItem : HandheldItem {
-    public override void Action(PuzzleObstacle obstacle)
-    {
-        if (obstacle == null) {
-            return;
-        }
-
-        if (obstacle.GetType() == typeof(DoorPuzzleObstacle)) {
-            obstacle.Action(player);
-        }
-    }
+﻿class KeyHandheldItem : HandheldItem {
 }
--- unity/Assets/Script/Puzzle/HandheldItem/WrenchHandheldItem.cs
+++ unity/Assets/Script/Puzzle/HandheldItem/WrenchHandheldItem.cs
@@ -1,14 +1,2 @@
-﻿using UnityEngine;
-
-class WrenchHandheldItem : HandheldItem {
-    public override void Action(PuzzleObstacle obstacle)
-    {
-        if (obstacle == null) {
-            return;
-        }
-
-        if (obstacle.GetType() == typeof(LockerPuzzleObstacle)) {
-            obstacle.Action(player);
-        }
-    }
+﻿class WrenchHandheldItem : HandheldItem {
 }
