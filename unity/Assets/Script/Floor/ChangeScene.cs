8903f58d3b495c0fa2fd498bcbcb146917234cf2
todd032 <todd032@naver.com>
2016-01-31 10:24:20 +0900

    add scene

---
+++ unity/Assets/Script/Floor/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string SceneName = "Stage_RoofToBasement";
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            //load scene
+            Application.LoadLevel(SceneName);
+        }
+    }
+
+}
