facbd1dc84bf45790780d4d7810705a88db7c58d
todd032 <todd032@naver.com>
2016-01-31 10:02:54 +0900

    locker work

--- unity/Assets/Script/Puzzle/Obstacle/KeyLockerPuzzleObstacle.cs
+++ unity/Assets/Script/Puzzle/Obstacle/KeyLockerPuzzleObstacle.cs
@@ -6,18 +6,24 @@ class KeyLockerPuzzleObstacle : PuzzleObstacle {
 
     public KeyHandheldItem item;
     public string ErrorText;
+    private string IsEmptyText = "Locker is empty";
+
+    public GameObject LockedGraphic;
+    public GameObject UnlockedGraphic;
 
     protected override void Start()
     {
         base.Start();
         playerUI = GameObject.FindWithTag("UI").GetComponent<PlayerUI>();
+        LockedGraphic.gameObject.SetActive(true);
+        UnlockedGraphic.gameObject.SetActive(false);
     }
 
     public override bool CheckIfActionIsPossible(Player player)
     {
-        if (item == null) {
-            return false;
-        }
+        //if (item == null) {
+        //    return false;
+        //}
 
         if (player.HandheldItem == null || player.HandheldItem.GetType() != typeof(WrenchHandheldItem)) {
             playerUI.SetErrorMessage(ErrorText, 2);
@@ -29,7 +35,16 @@ class KeyLockerPuzzleObstacle : PuzzleObstacle {
 
     public override void Action(Player player)
     {
-        item.GetPickedUp(player);
+        if(item == null)
+        {
+            playerUI.SetErrorMessage(IsEmptyText, 2);
+        }
+        if (item != null)
+        {
+            item.GetPickedUp(player);
+        }
         item = null;
+        UnlockedGraphic.gameObject.SetActive(true);
+        LockedGraphic.gameObject.SetActive(false);
     }
 }
