34d59dabbd047753e14f8e9e0be736a22e239670
Samuli Jaaskelainen <jaaskelainen.samuli@gmail.com>
2016-01-31 13:04:06 +0900

    Title splash.

--- unity/Assets/Script/UI/Title.cs
+++ unity/Assets/Script/UI/Title.cs
@@ -12,13 +12,43 @@ public class Title : MonoBehaviour
     float timer;
     float rate = 0.6f;
 
+    bool isSplashing;
+    public Image spash;
+
     void Start()
     {
         image = GetComponent<Image>();
+        StartCoroutine("SplashAnim");
+    }
+
+    IEnumerator SplashAnim()
+    {
+        isSplashing = true;
+
+        yield return new WaitForSeconds(3.0f);
+
+        float v = 1.0f;
+
+        while(v > 0.0f)
+        {
+            if(spash != null)
+                spash.color = new Color(1.0f, 1.0f, 1.0f, Mathf.Clamp01(v));
+
+            v -= Time.deltaTime * 3.0f;
+            yield return null;
+        }
+
+        if(spash != null)
+            spash.gameObject.SetActive(false);
+
+        isSplashing = false;
     }
 
     void Update ()
     {
+        if(isSplashing)
+            return;
+
 	    if(Time.time > timer)
         {
             timer = Time.time + rate;
