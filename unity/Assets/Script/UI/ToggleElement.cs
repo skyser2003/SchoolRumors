b9ce53d6dcaed0ff367cb374bb6d9cfd4ab4ab05
Samuli Jaaskelainen <jaaskelainen.samuli@gmail.com>
2016-01-31 09:56:05 +0900

    Title screen audio fixes.

--- unity/Assets/Script/UI/ToggleElement.cs
+++ unity/Assets/Script/UI/ToggleElement.cs
@@ -4,20 +4,23 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class ToggleElement : MonoBehaviour
 {
-    private bool visible = false; //show or hide at start
     public GameObject go;
+    public AudioSource audioSource;
 
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(() => {
             OnClick();
         });
-        OnClick(); //set initial state
     }
 
     void OnClick()
     {
-        visible = !visible;
-        go.SetActive(!visible);
+        if(audioSource != null)
+        {
+            audioSource.Play();
+        }
+
+        go.SetActive(!go.activeSelf);
     }
 }
